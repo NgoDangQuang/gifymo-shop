1f8ad38a40fc373f6414263eb0367f11f0d4589d
ngoquang336 <ngoquang336@gmail.com>
2022-09-11 19:59:09 +0700

    add FAQ page

--- src/features/faq/index.jsx
+++ src/features/faq/index.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import { Grid } from '@mui/material';
+import { FAQ } from 'api/FAQ';
+import { ItemsFaq } from './itemFAQ';
+import './styles.css';
 
 export function Faq(props) {
+  const data = FAQ;
+  const lengthData = data.length / 2;
+
   return (
-    <div>
-      <h2>FAQ</h2>
+    <div className="root-our-team">
+      <div className="container-our-team">
+        <div className="layout-our-team-page border-bottom-background">
+          <div className="title-our-team flex j-center ">
+            <h4 className="border-bottom-background">Most Popular Questions</h4>
+          </div>
+          <div className="layout-faq">
+            <Grid container spacing={{ xs: 4, md: 4 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+              <Grid item md={6}>
+                <Grid container spacing={{ md: 4 }}>
+                  {data.map(
+                    (item, index) =>
+                      index < lengthData && (
+                        <Grid item xs={2} sm={6} md={12}>
+                          <ItemsFaq dataItem={item} />
+                        </Grid>
+                      )
+                  )}
+                </Grid>
+              </Grid>
+
+              <Grid item md={6}>
+                <Grid container spacing={{ md: 4 }}>
+                  {data.map(
+                    (item, index) =>
+                      index >= lengthData && (
+                        <Grid item xs={2} sm={6} md={12}>
+                          <ItemsFaq dataItem={item} />
+                        </Grid>
+                      )
+                  )}
+                </Grid>
+              </Grid>
+            </Grid>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/features/faq/itemFAQ.jsx
@@ -0,0 +1,22 @@
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { useState } from 'react';
+import './styles.css';
+
+export function ItemsFaq(dataItem) {
+  const [visible, setVisible] = useState(false);
+  const data = dataItem.dataItem;
+
+  return (
+    <div className="layout-faq-content">
+      <div className="flex j-between" onClick={() => setVisible(!visible)}>
+        <h3>{data.question}</h3>
+        {!visible && (
+          <ArrowForwardIosIcon fontSize="small" sx={{ color: 'gray', fontSize: '16px' }} />
+        )}
+        {visible && <KeyboardArrowUpIcon sx={{ color: 'gray', fontSize: '26px' }} />}
+      </div>
+      {visible && <h4>{data.answer}</h4>}
+    </div>
+  );
+}
